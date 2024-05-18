2650e1bdcaf74a8ab2e46dc2316d95f3fd9964a7
Nikita Shunaev <shunayev@yandexlyceum.ru>
2024-05-18 22:11:21 +0500

    sidebar open close done

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { FormEvent, useEffect, useRef, useState } from 'react';
 import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
 import clsx from 'clsx';
@@ -7,15 +7,14 @@ import { Separator } from '../separator';
 import { Text } from '../text';
 import { Select } from '../select';
 import {
-	OptionType,
 	backgroundColors,
 	contentWidthArr,
-	defaultArticleState,
 	fontColors,
 	fontFamilyOptions,
 } from 'src/constants/articleProps';
 
 export const ArticleParamsForm = () => {
+	const formRef = useRef<HTMLDivElement | null>(null);
 	const [isOpen, setIsOpen] = useState(false);
 
 	const changeOpenState = () => {
@@ -24,17 +23,32 @@ export const ArticleParamsForm = () => {
 
 	// const [formState, setFormState] = useState(defaultArticleState);
 
-	const change = (selected: OptionType) => {
-		console.log(selected, defaultArticleState);
+	// const change = (selected: OptionType) => {
+	// 	console.log(selected, defaultArticleState);
+	// };
+
+	const toggleSidebarByClick = (event: MouseEvent) => {
+		if (isOpen && !formRef.current?.contains(event.target as Node)) {
+			setIsOpen(false);
+		}
 	};
 
+	const handleSubmit = (event: FormEvent) => {
+		event.preventDefault();
+	};
+
+	useEffect(() => {
+		document.addEventListener('click', toggleSidebarByClick);
+		return () => document.removeEventListener('click', toggleSidebarByClick);
+	}, [isOpen]);
+
 	return (
-		<>
+		<div ref={formRef}>
 			<ArrowButton isOpen={isOpen} setIsOpen={changeOpenState} />
 			{isOpen && (
 				<aside
 					className={clsx(styles.container, isOpen && styles.container_open)}>
-					<form className={styles.form}>
+					<form className={styles.form} onSubmit={handleSubmit}>
 						<Text size={31} weight={800} uppercase>
 							задайте параметры
 						</Text>
@@ -43,7 +57,7 @@ export const ArticleParamsForm = () => {
 						<Select
 							selected={fontFamilyOptions[0]}
 							options={fontFamilyOptions}
-							onChange={change}
+							// onChange={change}
 							title='шрифт'
 						/>
 						<div style={{ height: '50px' }}></div>
@@ -59,7 +73,7 @@ export const ArticleParamsForm = () => {
 						<Select
 							selected={fontColors[0]}
 							options={fontColors}
-							onChange={change}
+							// onChange={change}
 							title='цвет шрифта'
 						/>
 						<div style={{ height: '20px' }}></div>
@@ -84,6 +98,6 @@ export const ArticleParamsForm = () => {
 					</form>
 				</aside>
 			)}
-		</>
+		</div>
 	);
 };
